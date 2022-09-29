31ace352ef142359c558ccbcb4ab08e9834d4379
Michaela <malitherland@gmail.com>
2022-09-29 16:55:15 -0400

    exercise 6.9

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
+
+import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
 import voteReducer from './reducers/anecdoteReducer'
+import notificationReduce from './reducers/notificationReducer'
+const store = configureStore({
+  reducer: {
+    vote: voteReducer,
+    notification: notificationReducer
+  }
+})
 
-const store = createStore(voteReducer)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
